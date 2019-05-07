dd1510fcdf66a8eb1fed6c61f921947f14ded1fa
tvieregge <tmvieregge@gmail.com>
2019-05-06 23:18:56 -0400

    applying vector forces

--- structure.py
+++ structure.py
@@ -1,14 +1,12 @@
 import math
-import numpy
+import numpy as n
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 class Point:
-    force = 0
     def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+        self.l = n.array([x,y,z])
+        self.force = n.array([0.0,0.0,0.0])
 
 
 class Edge:
@@ -22,20 +20,32 @@ def start(fname):
         content = f.readlines()
         edges = parse_points(content)
 
-        points = [Point(1,1,1), Point(3,1,1)]
-        edges = [Edge(points[0],points[1])]
+        points = [Point(1.0,1.0,1.0), Point(3.0,1.0,1.0), Point(1.5, 1.5, 1.0)]
+        edges = [Edge(points[0],points[1]), Edge(points[0], points[2]), Edge(points[1], points[2])]
 
         fig = plt.figure()
         ax = fig.gca(projection='3d')
 
         for e in edges:
-            ax.plot([e.p1.x, e.p2.x], [e.p1.y, e.p2.y], [e.p1.z+1, e.p2.z+1])
+            ax.plot([e.p1.l[0], e.p2.l[0]], [e.p1.l[1], e.p2.l[1]], [e.p1.l[2]+1, e.p2.l[2]+1])
 
         calc_forces(edges, points)
         apply_forces(points)
 
         for e in edges:
-            ax.plot([e.p1.x, e.p2.x], [e.p1.y, e.p2.y], [e.p1.z, e.p2.z])
+            ax.plot([e.p1.l[0], e.p2.l[0]], [e.p1.l[1], e.p2.l[1]], [e.p1.l[2], e.p2.l[2]])
+
+        calc_forces(edges, points)
+        apply_forces(points)
+
+        for e in edges:
+            ax.plot([e.p1.l[0], e.p2.l[0]], [e.p1.l[1], e.p2.l[1]], [e.p1.l[2]+2, e.p2.l[2]+2])
+
+        calc_forces(edges, points)
+        apply_forces(points)
+
+        for e in edges:
+            ax.plot([e.p1.l[0], e.p2.l[0]], [e.p1.l[1], e.p2.l[1]], [e.p1.l[2]+3, e.p2.l[2]+3])
 
         plt.show()
 
@@ -45,31 +55,39 @@ def calc_forces(edges, points):
         for other_p in points:
             if p == other_p:
                 continue
-            p.force = -calc_repulsion(p, other_p)
+            p.force = calc_repulsion(p, other_p)
             print(p.force)
-    print('---')
+    print('--')
 
     for e in edges:
         attraction = calc_attraction(e)
-        e.p1.force += attraction
+        e.p1.force -= attraction
         e.p2.force += attraction
         print(e.p1.force)
         print(e.p2.force)
+    print('----')
 
 def apply_forces(points):
     for p in points:
-        p.x += p.force
+        p.l += (0.2) * p.force
 
 def calc_dist(p1, p2):
-    return math.hypot(p1.x - p2.x, p1.y - p2.y)
+    diff = p1.l - p2.l
+    return (diff**2).sum()**0.5
 
 
 def calc_repulsion(p1, p2):
-    return 1/(calc_dist(p1, p2)**2)
+    dist = calc_dist(p1, p2)
+    unit = ((p1.l-p2.l)/dist)
+    return  unit/(dist**2)
 
 
 def calc_attraction(edge):
-    return calc_dist(edge.p1, edge.p2)
+    p1 = edge.p1
+    p2 = edge.p2
+    dist = calc_dist(p1, p2)
+    unit = ((p1.l-p2.l)/dist)
+    return dist * unit
 
 
 def parse_points(lines):
