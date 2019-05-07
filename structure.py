9eee9a4a784223f729673347641427d33103855d
tvieregge <tmvieregge@gmail.com>
2019-05-07 01:23:58 -0400

    bare bones 3d animation

--- structure.py
+++ structure.py
@@ -4,14 +4,35 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
 
+class Point:
+    def __init__(self, x, y, z):
+        self.l = np.array([x,y,z])
+        self.force = np.array([0.0,0.0,0.0])
+
+
+class Edge:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
 fig = plt.figure()
-ax = plt.axes(xlim=(0,5), ylim=(0,5))
+ax = Axes3D(fig, xlim=(0,5), ylim=(0,5))
+# ax = fig.gca(projection='3d', xlim=(0,5), ylim=(0,5))
+# ax = plt.axes()
+# ax = plt.axes(xlim=(0,5), ylim=(0,5))
 # for e in edges:
-line, = ax.plot([],[], lw=2)
+line, = ax.plot([1,2],[3,4], lw=2)
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# # for e in edges:
+# line, = ax.plot([],[])
+g_points = [Point(1.0,1.0,1.0), Point(3.0,1.0,1.0), Point(1.5, 1.5, 1.0)]
+g_edges = [Edge(g_points[0],g_points[1]), Edge(g_points[0], g_points[2]), Edge(g_points[1], g_points[2])]
 
 
 def init():
-    line.set_data([], [])
+    line.set_data([1,2], [3,4])
     return line,
 
 def animate(i):
@@ -22,6 +43,7 @@ def animate(i):
     # line.set_data([g_edges[0].p1.l[0], g_edges[0].p2.l[0]], [g_edges[0].p1.l[1], g_edges[0].p2.l[1]])
     print('Tick')
     line.set_data([1,2],[1,3])
+    line.set_3d_properties([0,0])
     return line,
 
 if __name__ == '__main__':
